beb0e160473d0f8683f64f34c2db0e52006ba255
yohan3103 <yohan.martisa.tif17@polban.ac.id>
2020-10-08 02:11:51 +0700

    revisi

---
+++ index.js
@@ -0,0 +1,5 @@
+function namafungsi (){	
+	return "a";
+}
+
+module.exports = namafungsi;
---
+++ tests/index.test.js
@@ -0,0 +1,4 @@
+var namafungsi = require('../index.js')
+test('namafungsi', function () {
+	expect(namafungsi()).toBe('a');
+})
